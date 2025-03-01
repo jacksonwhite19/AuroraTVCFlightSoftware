f67544e660371ebff98d824c3e45fc4dab632061
Jackson White <jacksonwhite19@icloud.com>
2025-03-01 12:15:03 -0600

    updated drivers, was able to get USB serial working after DFU upload

--- Connection Testing/MatekConnectionTest/MatekConnectionTest.ino
+++ Connection Testing/MatekConnectionTest/MatekConnectionTest.ino
@@ -1,20 +1,32 @@
-// Include the pinout definitions (ensure this file is in your project folder)
 #include "MatkeH743_Pinout.h"
+// No need to include USBSerial.h or create SerialUSB object - it's already defined
 
 void setup() {
-    // Initialize LED pins as outputs
+    // Initialize pins
     pinMode(LED1_PIN, OUTPUT);
     pinMode(LED2_PIN, OUTPUT);
+    
+    // Initialize USB serial
+    SerialUSB.begin();
+    
+    // Optional: Wait for serial connection with timeout
+    uint32_t startTime = millis();
+    while (!SerialUSB && millis() - startTime < 3000) {
+        delay(10);
+    }
+    
+    SerialUSB.println("MatekH743 initialized!");
+    SerialUSB.println("LED blinking started...");
 }
 
 void loop() {
-    // Turn LED1 on, LED2 off
     digitalWrite(LED1_PIN, HIGH);
     digitalWrite(LED2_PIN, LOW);
+    SerialUSB.println("LED1 ON, LED2 OFF");
     delay(500);
-
-    // Turn LED1 off, LED2 on
+    
     digitalWrite(LED1_PIN, LOW);
     digitalWrite(LED2_PIN, HIGH);
+    SerialUSB.println("LED1 OFF, LED2 ON");
     delay(500);
 }
